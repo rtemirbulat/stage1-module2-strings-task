4007977d7b813724029bc510d7234526306822fd
rtemirbulat <equalityisnotequality@gmail.com>
2024-02-29 19:28:33 +0600

    StringSplitter issue

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,30 @@
 package com.epam.mjc;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class StringSplitter {
 
     /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     * Splits given string applying all delimiters to it. Keeps order of result substrings as in source string.
      *
      * @param source source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> resultList = new ArrayList<>();
+        List<String> tempArr = new ArrayList<>();
+        String temp = source;
+        for (String d: delimiters) {
+            StringTokenizer st1 = new StringTokenizer(temp, d);
+            while (st1.hasMoreTokens()){
+                tempArr.add(st1.nextToken());
+            }
+            temp = String.join(", ",tempArr);
+            tempArr.clear();
+        }
+        resultList.add(temp);
+
+        return  resultList;
     }
 }
